be3db5359cb6a00eb26a9c723de5a294c26c3f14
“gagamot” <shakurovainfo@gmail.com>
2020-08-12 15:17:39 -0400

    update logic for 5 rounds

--- script.js
+++ script.js
@@ -7,31 +7,38 @@ let winner;
 
 const logMessage = document.querySelector("#logmessage");
 const pRound = document.querySelector("#round");
-const pPlayer = document.querySelector("#pc");
-const pComputer = document.querySelector("#cc");
+const pPlayerScore = document.querySelector("#pc");
+const pComputerScore = document.querySelector("#cc");
 const btn = document.querySelectorAll("button");
-  
+const divStart = document.querySelector("#divStart");
 
 btn.forEach((button) => {
         button.addEventListener("click", () => {
-            
             playRound(button.value,computerPlay());
             pRound.textContent = ++round;
-            if (round == 5) {
-                game(playerScore, computerScore);
-                btn.forEach((button) => {
-                    button.disabled = true
-                    //need to disable hover as well
-                });
-
-                round = 0;
+            if (round === 5) {
+                btn.forEach((button) => button.disabled = true);  //need to disable hover as well
+                winGame(playerScore, computerScore);
                 startRound();
-                }
+            }            
         });
 });
 
+function startRound() {
+    divStart.style.display = "block";
+    btn.forEach((button) => button.disabled = true);
+    divStart.addEventListener("click", () => {
+    pPlayerScore.textContent = playerScore = 0;
+        pComputerScore.textContent = computerScore = 0;
+        pRound.textContent = round = 0;
+        btn.forEach((button) => button.disabled = false);
+        divStart.style.display = "none"
+        
+        logMessage.textContent = "";
+    });
 
-
+    
+}
 
 function computerPlay() {
     let computerChoice = ["Scissors", "Rock", "Paper" ];
@@ -41,43 +48,27 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     let message = "";
     if (playerSelection === computerSelection) {
-        message = "Wow, Even-Steven! Please play again!";
+        message = "Even-Steven!";
         logMessage.textContent = message;
     } else if (
         ((playerSelection === "Rock") && (computerSelection === "Scissors")) ||
         ((playerSelection === "Paper") && (computerSelection === "Rock")) ||
         ((playerSelection === "Scissors") && (computerSelection === "Paper"))
     ) { 
-        pPlayer.textContent = ++playerScore;
+        pPlayerScore.textContent = ++playerScore;
         message = `You won! ${playerSelection} beats ${computerSelection}.`;
         logMessage.textContent = message;
     } else {
         message = `You lost! ${playerSelection} doesn't beat ${computerSelection}.`;
-        pComputer.textContent = ++computerScore;
+        pComputerScore.textContent = ++computerScore;
         logMessage.textContent = message;
     }            
 }
 
-function game(playerScore, computerScore) {
+function winGame(playerScore, computerScore) {
     if (playerScore == computerScore) winner = " You should try again!";
     else if (playerScore > computerScore) winner = " You won the game!";
-    else winner = " You lost!"
+    else winner = " You lost this time, try again!"
     logMessage.textContent = "GAME OVER!" + winner;
 }
 
-function startRound() {
-    const btnStart = document.createElement("button");
-    btnStart.classList.add("btnStart");
-    btnStart.textContent = "Play again?";
-    const divLog = document.querySelector("#logmessage");
-    divLog.appendChild(btnStart);
-
-    btnStart.addEventListener("click", () => {
-        playerScore = 0;
-        computerScore = 0;
-
-
-    });
-
-
-}
